00e25a1aeb92b54f31b8598130ce5b2d9fc3c78c
Arianne Marie Angela Luvite <ariannemarieangela.luvite@cit.edu>
2023-11-27 02:33:11 +0800

    Add project files.

---
+++ Subtraction/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Subtraction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.loadImage = new System.Windows.Forms.Button();
+            this.loadbackground = new System.Windows.Forms.Button();
+            this.subtract = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(113, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(322, 391);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(515, 62);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(322, 391);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(918, 62);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(322, 391);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // loadImage
+            // 
+            this.loadImage.Location = new System.Drawing.Point(178, 483);
+            this.loadImage.Name = "loadImage";
+            this.loadImage.Size = new System.Drawing.Size(173, 56);
+            this.loadImage.TabIndex = 3;
+            this.loadImage.Text = "Load Image";
+            this.loadImage.UseVisualStyleBackColor = true;
+            this.loadImage.Click += new System.EventHandler(this.loadImage_Click);
+            // 
+            // loadbackground
+            // 
+            this.loadbackground.Location = new System.Drawing.Point(593, 483);
+            this.loadbackground.Name = "loadbackground";
+            this.loadbackground.Size = new System.Drawing.Size(173, 56);
+            this.loadbackground.TabIndex = 4;
+            this.loadbackground.Text = "Load Background";
+            this.loadbackground.UseVisualStyleBackColor = true;
+            this.loadbackground.Click += new System.EventHandler(this.loadbackground_Click);
+            // 
+            // subtract
+            // 
+            this.subtract.Location = new System.Drawing.Point(996, 483);
+            this.subtract.Name = "subtract";
+            this.subtract.Size = new System.Drawing.Size(173, 56);
+            this.subtract.TabIndex = 5;
+            this.subtract.Text = "Subtract";
+            this.subtract.UseVisualStyleBackColor = true;
+            this.subtract.Click += new System.EventHandler(this.subtract_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1346, 599);
+            this.Controls.Add(this.subtract);
+            this.Controls.Add(this.loadbackground);
+            this.Controls.Add(this.loadImage);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button loadImage;
+        private System.Windows.Forms.Button loadbackground;
+        private System.Windows.Forms.Button subtract;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ Subtraction/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Subtraction
+{
+    public partial class Form1 : Form
+    {
+        Bitmap imageB, imageA, colorgreen, resultImage;
+
+        private void loadImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp|All Files|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) { }
+        }
+
+        private void loadbackground_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp|All Files|*.*";
+
+            if (openFileDialog2.ShowDialog() == DialogResult.OK) { }
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            imageA = new Bitmap(openFileDialog2.FileName);
+            pictureBox2.Image = new Bitmap(imageA);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            imageB = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = new Bitmap(imageB);
+        }
+
+        private void subtract_Click(object sender, EventArgs e)
+        {
+            Color mygreen = Color.FromArgb(0, 0, 255);
+            int greygreen = (mygreen.R + mygreen.G + mygreen.B) / 3;
+            int threshold = 5;
+            resultImage = new Bitmap(imageA.Width, imageA.Height);
+
+            
+            for (int x = 0; x < imageB.Width; x++)
+            {
+                for (int y = 0; y < imageB.Height; y++)
+                {
+                    Color pixel = imageB.GetPixel(x, y);
+                    Color backpixel = imageA.GetPixel(x, y);
+
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    int subtractvalue = Math.Abs(grey - greygreen);
+
+                    if (subtractvalue > threshold)
+                    {
+                        Console.WriteLine($"Position ({x}, {y}):");
+                        Console.WriteLine($"  ImageA: {backpixel}");
+                        Console.WriteLine($"  ImageB: {pixel}");
+                        Console.WriteLine($"  SubtractValue: {subtractvalue}");
+                        resultImage.SetPixel(x, y, backpixel);
+                    }  else
+                        resultImage.SetPixel(x, y, pixel);
+                }
+                
+            }
+
+            pictureBox3.Image = resultImage;
+        }
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
